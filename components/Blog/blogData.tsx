0c8ffe579c573ed684f3188f9b18ef3dff6ed37b
Samy <samyallal2@gmail.com>
2024-02-08 15:47:02 -0800

    [Added] projects

--- components/Blog/blogData.tsx
+++ components/Blog/blogData.tsx
@@ -3,35 +3,36 @@ import { Blog } from "@/types/blog";
 const BlogData: Blog[] = [
   {
     _id: 1,
-    mainImage: "/images/blog/projects/1-1.png",
-    title: "Business Web Platform",
-    link: "https://www.framemax.com/",
+    mainImage: "/images/blog/projects/ai-sheets.png",
+    title: "AI-Powered Google Sheets",
+    link: "https://www.loom.com/share/52c5d7922a9a4078a0de12e2e30b8746?sid=46e08920-a404-42b5-8dfd-d1e42985cdc4",
     metadata:
-      "Framemax",
+      "Google Sheets x AI",
   },
   {
     _id: 2,
-    mainImage: "/images/blog/projects/2.png",
-    title: "ChatBot Integration",
-    link: "https://www.samyallal.com/",
+    mainImage: "/images/blog/projects/live-customer.png",
+    title: "AI-Powered Chatbot",
+    link: "https://www.loom.com/share/58145328aefb49cdbc694f7b5ccbbcb7?sid=cc4d7a9d-d465-4781-baa1-b0b93ba109ef",
     metadata:
-      "Portfolio",
+      "AI-Powered Customer Service Chatbot",
+  },
+  {
+    _id: 3,
+    mainImage: "/images/blog/projects/ai-image.png",
+    title: "AI Image Generation",
+    metadata:
+      "AI Image Generation & Prompting",
+  },
+  {
+    _id: 4,
+    mainImage: "/images/blog/projects/2.png",
+    title: "AI-ChatBot Web Integration",
+    link: "www.samyallal.com",
+    metadata:
+      "AI-Powered Virtual Assistant",
   },
   /*{
-    _id: 2,
-    mainImage: "/images/blog/blog-03.png",
-    title: "Tips to quickly improve your coding speed.",
-    metadata:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit convallis tortor.",
-  },
-  {
-    _id: 1,
-    mainImage: "/images/blog/blog-03.png",
-    title: "Free advertising for your online business",
-    metadata:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit convallis tortor.",
-  },
-  {
     _id: 2,
     mainImage: "/images/blog/blog-04.png",
     title: "9 simple ways to improve your design skills",
