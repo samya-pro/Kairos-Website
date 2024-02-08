0373a57214b62f2703623c4da5cc157ae01cd9aa
Samy <samyallal2@gmail.com>
2024-02-07 22:51:44 -0800

    [Deploy] #11

--- components/Features/SingleFeature.tsx
+++ components/Features/SingleFeature.tsx
@@ -25,6 +25,7 @@ const SingleFeature = ({ feature }: { feature: Feature }) => {
         transition={{ duration: 0.5 }}
         viewport={{ once: true }}
         className="animate_top z-40 rounded-lg border border-white bg-white p-7.5 shadow-solid-3 transition-all hover:shadow-solid-4 dark:border-strokedark dark:bg-blacksection dark:hover:bg-hoverdark xl:p-12.5"
+        style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}
       >
         <div className="relative flex h-16 w-16 justify-center items-center rounded-[4px] bg-primary">
           <Image src={icon} width={36} height={36} alt="title" />
